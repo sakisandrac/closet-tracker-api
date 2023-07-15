8372a12714e5613756f2e4502bcb39f2f5e84991
“Saki <sakisandra.c@gmail.com>
2023-07-15 16:25:40 -0600

    Initial commit

---
+++ data/data.js
@@ -0,0 +1,58 @@
+module.exports = [
+  {
+    pants: [
+      {
+        id: 1,
+        image: ""
+      }
+    ],
+    shirts: [
+      {
+        id: 1,
+        image: ""
+      }
+    ],
+    skirts: [
+      {
+        id: 1,
+        image: ""
+      }
+    ],
+    onePieces: [
+      {
+        id: 1,
+        image: ""
+      }
+    ],
+    shoes: [
+      {
+        id: 1,
+        image: ""
+      }
+    ],
+    bags: [
+      {
+        id: 1,
+        image: ""
+      }
+    ],
+    accessories: [
+      {
+        id: 1,
+        image: ""
+      }
+    ],
+    outerwear: [
+      {
+        id: 1,
+        image: ""
+      }
+    ],
+    misc: [
+      {
+        id: 1,
+        image: ""
+      }
+    ],
+  } 
+]
---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3003;
+const app = express();
+const data = require('./data/data');
+
+app.locals = {
+  title: 'Data',
+  data,
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/v1/data', (req, res) => {
+  const { data } = app.locals;
+  res.status(200).json({ data });
+});
+
+app.post('/api/v1/data', (req, res) => {
+  const { title, description, id } = req.body;
+  const requiredProperties = ['title', 'description'];
+
+  // for (let requiredParameter of requiredProperties) {
+  //   if (req.body[requiredParameter] === undefined) {
+  //     return res.status(422).json({
+  //       message: `You are missing a required parameter of ${requiredParameter}`
+  //     });
+  //   }
+  // }
+
+    // Check that the userID is a number
+    // if (typeof userID !== 'number') {
+    //   return res.status(422).json({
+    //     message: `Invalid userID data type. userID must be a number.`
+    //   })
+    // }
+
+    app.locals.data.push({ id, title, description })
+    res.status(201).json({ 
+      message: "",
+      // newData: {
+      //   id, 
+      //   title,
+      //   description,
+      // }
+    });
+})
+
+// app.delete('/api/v1/data/:id', (req, res) => {
+//   const  { id } = req.params;
+//   const { data } = app.locals;
+
+//   const filteredIdeas = ideas.filter(idea => {
+//     return idea.id !== parseInt(id)
+//   })
+
+//   app.locals.ideas = filteredIdeas;
+
+//   res.status(200).json({
+//     message: `Idea #${id} has deleted`,
+//     ideas: filteredIdeas
+//   })
+// })
+
+app.listen(port, () => {
+  console.log(`${app.locals.title} is now running on http://localhost:${port} !`)
+});
